f241cb72a582f845d0736be73448392bc032650e
siva1907 <127549398+siva1907@users.noreply.github.com>
2023-04-24 10:23:43 +0530

    first commit

--- src/app/userstore/nav/NavComponent.ts
+++ src/app/userstore/nav/NavComponent.ts
@@ -29,7 +29,7 @@ export class NavComponent {
 
   viewCustomer()
   {
-    this.routeing.routerLink=`/orders/${this.customer.id}`;
+   // this.routeing.routerLink=`/orders/${this.customer.id}`;
   }
 
   showDropdown() {
